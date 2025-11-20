6b472076a577b70574d71d0b16af7c4a7a15958c
Dev Dixit <devdixitsocial@gmail.com>
2025-11-20 16:07:34 +0530

    add: validator for login or register, verify the OTP

---
+++ src/validators/auth.validator.ts
@@ -0,0 +1,16 @@
+import Joi from "joi";
+
+export const registerOrLoginSchema = Joi.object({
+  mobileNumber: Joi.number().min(10).required().messages({
+    "number.empty": 'Mobile number is required',
+    "number.min": 'Mobile number should have at least 10 numbers'
+  })
+});
+
+export const verifyOtp = Joi.object({
+  otp: Joi.number().min(6).max(8).required().messages({
+    "number.empty": 'Verification OTP is required',
+    "number.min": 'Verification OTP should have at least 6 numbers',
+    "number.max": 'Verification OTP should not over exceed 8 numbers'
+  })
+});
