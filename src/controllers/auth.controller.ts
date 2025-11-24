09dc66d0bf5cef5b5f1f88252182abea1bafd302
Dev Dixit <devdixitsocial@gmail.com>
2025-11-24 20:21:55 +0530

    remove: verification logic from register

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from 'express'
+import bcrypt from 'bcryptjs';
 
 import { makeLogFile } from '../utils/logger'
 import User from '../models/user.model';
-import RedisClient from '../config/redis.config';
-import { signJwt } from '../services/jwt.service';
 import SendEmail from '../services/email.service';
 
 export const authInit = async (req: Request, res: Response) => {
@@ -18,34 +17,27 @@ export const authInit = async (req: Request, res: Response) => {
       });
     }
 
-    const otp = Math.floor(100000 + Math.random() * 900000);
-
-    const payload = { name, email, password, otp: String(otp) };
-    const key = signJwt(JSON.stringify(payload), '2m');
-
-    await RedisClient.set(`${key}`, JSON.stringify(payload), "EX", 120)
-
     setTimeout(async () => {
+      const hashPassword = await bcrypt.hash(password, 12);
+
+      await User.create({
+        name, email, password: hashPassword 
+      });
+
       await SendEmail(
         email,
-        `Welcome to the E-commerce API - Verification`,
-        `Kindly verify your account. Your verification OTP is ${otp}.`
+        `Welcome Email`,
+        `Welcome ${name} to the E-commerce API. Your account is successfully associated with email ${email}.`
       );
 
       const entry = `\n[${new Date().toISOString()}] Auth init -> IP: ${req.ip} | Name: ${name} | Email: ${email}\n`
       makeLogFile("auth-init.log", entry);
     }, 3000);
 
-    res.cookie('v_token', key, {
-      httpOnly: true,
-      sameSite: 'lax',
-      secure: false
-    });
-
     return res.status(200).json({
       success: true,
-      message: `We've sent you the OTP for verification. kindly check your email to verify your account.`
-    })
+      message: `Welcome to the E-commerce API. Your account is created. You can do login now.`
+    });
   }
   catch (error) {
     const entry = `\n[${new Date().toISOString()}] Error in auth init -> ${req.ip}\n`
