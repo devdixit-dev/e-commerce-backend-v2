3b945a0fbc314b1b5fe15d4c7f2581a8890374a1
Dev Dixit <devdixitsocial@gmail.com>
2025-11-30 20:01:02 +0530

    add: multer middleware, check point for upload with multer, avatar field in user model, test done

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.route';
 import UserRouter from './routes/user.route';
 import auth from './middlewares/auth.middleware';
+import upload from './middlewares/multer.middleware';
+import User from './models/user.model';
 
 const createServer = async () => {
   const app = express();
@@ -26,6 +28,23 @@ const createServer = async () => {
 
   app.use('/api/auth', authRouter);
   app.use('/api/user', auth, UserRouter);
+
+  app.post("/upload", upload.single('avatar'), async (req, res) => {
+    if(!req.file){
+      return res.json({
+        message: 'File is required'
+      });
+    }
+
+    const user = await User.findByIdAndUpdate(
+      "692880b4bf6e89cc9fad46cb",
+      { avatar: req.file.originalname }
+    );
+
+    return res.json({
+      message: 'File uploaded'
+    });
+  })
 
   app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     console.error("Unhandled error:", err);
--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -1,10 +1,28 @@
 import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
 
-const storage = multer.memoryStorage();
+const uploadDir = path.join(process.cwd() + `../../../uploads`);
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const filename = `${Date.now()}-${file.originalname}`
+    cb(null, filename);
+  }
+});
 
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }
+  limits: {
+    fileSize: 5 * 1024 * 1024
+  }
 });
 
 export default upload;
